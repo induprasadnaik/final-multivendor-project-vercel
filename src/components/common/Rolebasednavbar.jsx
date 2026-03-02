5087a24f8c6088f790dfdc52a49ba302c44948ba
Developer Phitany <developer@phitany.com>
2026-03-02 07:42:32 +0530

    menu

--- src/components/common/Rolebasednavbar.jsx
+++ src/components/common/Rolebasednavbar.jsx
@@ -8,8 +8,8 @@ function Rolebasednavbar(props) {
      const { user, loading } = useAuth();
       if (loading) return null;
   if (!user) {
-      //return <CustomerNavbar {...props} />;
-      return null;
+      return <CustomerNavbar {...props} />;
+      // return null;
   };
 
   if (user.role === "admin" ) {
